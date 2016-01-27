6b947807b1395b0bab69989bd5ccbac2b9239366
Gwartney <davidg@boundary.com>
2016-01-26 16:08:28 -0800

    Place holder for the Plugin Metric

---
+++ src/main/java/com/bmc/truesight/plugins/activemq/PluginMetric.java
@@ -0,0 +1,10 @@
+package com.bmc.truesight.plugins.activemq;
+
+import java.text.ParseException;
+
+public class PluginMetric implements PluginParsedItem {
+
+    public void parse(String s) throws ParseException {
+
+    }
+}
